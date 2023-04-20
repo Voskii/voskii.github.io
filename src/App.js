a4bf05647a866f0f849d657bf5b1a0da8c49b65d
Kyle Shutt <xavior04350@yahoo.com>
2023-04-20 16:09:48 -0500

    1st draft

--- src/App.js
+++ src/App.js
@@ -19,8 +19,10 @@ export default function App() {
   return (
     <div className="container">
       <div className="header">
-        <AboutMe />
+        <Header />
+        
       </div>
+      <AboutMe />
         {projects.map(proj => <><ProjectCard data={proj} key={proj.name}/></>)}
       <div className="moveButt">
         <a href="https://www.linkedin.com/in/swingshutt/" target="_blank" rel="noreferrer">
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,10 @@
 import React from "react";
+import img from '../images/me.png';
 
-export default function aboutMe(){
+export default function header(){
     return (
-        <div className="header-aboutme-container">
-            <h1 className="headText">Kyle Shutt</h1>
-            <h1 className="aboutText">First off, HI DAD!. HELLO, I'M KYLE! I think humanity is in need of technology that is healthy with it's data practices, useful in their daily lives, and performs in ways that can help individuals develop healthy habits // lifestyles. I want to invest my time in ways to make someone's life better, way.. way better.  Please check back as I update this site with my projects. I will be open for work June. 1st 2023!</h1>
+        <div className="">
+            <div className="aboutText">First off, HI DAD!. HELLO, I'M KYLE! I think humanity is in need of technology that is healthy with it's data practices, useful in their daily lives, and performs in ways that can help individuals develop healthy habits // lifestyles. I want to invest my time in ways to make someone's life better, way.. way better.  Please check back as I update this site with my projects. I will be open for work June. 1st 2023!</div>
         </div>
     )
 }
