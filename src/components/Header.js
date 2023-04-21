db3436fee3e7a1f76120e0241620047354f39f8b
Kyle Shutt <xavior04350@yahoo.com>
2023-04-20 22:36:20 -0500

    name updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return (
         <div className="header-aboutme-container">
-            <h1 className="headText">Voski Deathbringer</h1>
+            <h1 className="headText">Kyle Shutt</h1>
             <h1 className="aboutText">Full Stack Web Developer // Father // Veteran</h1>
         </div>
     )
