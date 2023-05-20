cc315854689ccd3e5ed82952ff4ece2c22690a27
Kyle Shutt <xavior04350@yahoo.com>
2023-05-20 10:44:40 -0500

    about me update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ import img from '../images/me.png';
 export default function header(){
     return (
         <div className="">
-            <div className="aboutText">First off, hey Pops! HELLO, I'M Kyle! I think humanity is in need of technology that is healthy with it's data practices, useful in their daily lives, and performs in ways that can help individuals develop healthy habits // lifestyles. I want to invest my time in ways to make someone's life better, way.. way better.  Please check back as I update this site with my projects. I look forward to the possibility of interviewing with you!</div>
+            <div className="aboutText">First off, hey Pops! HELLO, I'M Kyle! I think humanity is in need of technology that is healthy with its data practices, useful in their daily lives and performs in ways that can help individuals develop healthy habits // lifestyles. I want to invest my time in ways to make someone's life better, way.. way better.  Please check back as I update this site with my projects. I look forward to the possibility of interviewing with you!</div>
         </div>
     )
 }
